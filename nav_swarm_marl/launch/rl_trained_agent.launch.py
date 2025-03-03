d1c5c8149d18af37a174aaec2f9149d4886ad63b
Sachin Kumar <sachinkum123567@gmail.com>
2025-03-03 20:45:03 +0100

    rl trained agent launch file updated

---
+++ nav_swarm_marl/launch/rl_trained_agent.launch.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='hospital_robot_spawner',
+            executable='trained_agent_manual_rviz',
+            name='trained_agent_tb3_0',
+            output='screen',
+            namespace='tb3_0',
+            # launch_prefix="/media/asus/backup/zzzzz/ros2/rtw_workspaces/humble_ws/rl/bin/python3",
+            # arguments=['0', '0', '0.01', '0', '0', '0', 'map', 'tb3_0/odom']
+        ),
+
+        Node(
+            package='hospital_robot_spawner',
+            executable='trained_agent_manual_rviz',
+            name='trained_agent_tb3_1',
+            output='screen',
+            namespace='tb3_1',
+            # launch_prefix="/media/asus/backup/zzzzz/ros2/rtw_workspaces/humble_ws/rl/bin/python3",
+            # arguments=['0', '0', '0.01', '0', '0', '0', 'map', 'tb3_0/odom']
+        ),
+        Node(
+            package='hospital_robot_spawner',
+            executable='trained_agent_manual_rviz',
+            name='trained_agent_tb3_2',
+            output='screen',
+            namespace='tb3_2',
+            # launch_prefix="/media/asus/backup/zzzzz/ros2/rtw_workspaces/humble_ws/rl/bin/python3",
+            # arguments=['0', '0', '0.01', '0', '0', '0', 'map', 'tb3_0/odom']
+        ),
+
+        Node(
+            package='hospital_robot_spawner',
+            executable='path_publisher',
+            name='path_publisher_tb3_0',
+            output='screen',
+            namespace='tb3_0',
+        ),
+        Node(
+            package='hospital_robot_spawner',
+            executable='path_publisher',
+            name='path_publisher_tb3_1',
+            output='screen',
+            namespace='tb3_1',
+        ),
+        Node(
+            package='hospital_robot_spawner',
+            executable='path_publisher',
+            name='path_publisher_tb3_2',
+            output='screen',
+            namespace='tb3_2',
+        ),
+    ])
